dec1c965b7ce556661c6d16ad0511ddead4a807d
milliee <112637496+millieeway@users.noreply.github.com>
2022-12-15 00:52:19 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.05 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sqrt = (int) Math.sqrt(n);
+        
+        for(int i = 1; i <= sqrt; i++){
+            if(n % i == 0){
+                answer++;
+                if(n / i != i)
+                    answer++;
+            }
+        }
+        return answer;
+    }
+}

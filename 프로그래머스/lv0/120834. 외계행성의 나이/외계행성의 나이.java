2f3133eec700f8ee10325003bc73350761e64aba
milliee <112637496+millieeway@users.noreply.github.com>
2022-12-14 22:37:25 +0900

    [level 0] Title: 외계행성의 나이, Time: 21.62 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        String age2 = Integer.toString(age);
+        for(int i = 0; i < age2.length(); i++){
+            answer += (char)(age2.charAt(i) + 49);
+        }
+        return answer;
+    }
+}

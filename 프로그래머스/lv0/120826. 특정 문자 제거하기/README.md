520eff121d8c1077eff605d2e68a2b9454571a9d
milliee <112637496+millieeway@users.noreply.github.com>
2022-12-14 20:23:22 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/README.md
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특정 문자 제거하기 - 120826 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120826) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120826?language=java) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 0.16 ms
+메모리: 78.4 MB, 시간: 0.05 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,11 +1,6 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        StringBuffer sb = new StringBuffer(my_string);
-        String str = sb.toString();
-        while(str.contains(letter)){
-            str = sb.deleteCharAt(sb.indexOf(letter)).toString();
-            if(str.contains(letter) == false) break;
-        }
-        return str;
+        String answer = my_string.replace(letter, "");
+        return answer;
     }
 }

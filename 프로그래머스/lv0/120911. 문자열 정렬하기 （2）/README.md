a064254bc34d285df5130860aa26f1930183bfc4
milliee <112637496+millieeway@users.noreply.github.com>
2023-01-05 11:23:23 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.37 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/README.md
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 정렬하기 (2) - 120911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120911) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120911?language=java) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.38 ms
+메모리: 74.7 MB, 시간: 0.37 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,12 +2,9 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-       
         char[] charArr = my_string.toLowerCase().toCharArray();
         Arrays.sort(charArr);
-        answer = String.valueOf(charArr);
-        
-        return answer;
+       
+        return new String(charArr);
     }
 }

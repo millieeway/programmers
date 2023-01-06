b2331ac4dc45c847e79b975c5896dfa3b3bb374d
milliee <112637496+millieeway@users.noreply.github.com>
2023-01-06 23:07:55 +0900

    [level 0] Title: OX퀴즈, Time: 0.12 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -1,20 +1,15 @@
 class Solution {
     public String[] solution(String[] quiz) {
         String[] answer = new String[quiz.length];
+        
         for(int i = 0; i < quiz.length; i++){
             int result = 0;
             String[] num = quiz[i].split(" ");
             result += Integer.parseInt(num[0]);
-            for(int j = 1; j < num.length; j++){
-                if(num[j].equals("+")){
-                    result += Integer.parseInt(num[j+1]);
-                    j++;
-                }else if(num[j].equals("-")){
-                    result -= Integer.parseInt(num[j+1]);
-                    j++;
-                }else if(num[j].equals("=")){
-                    break;
-                }
+            if(num[1].equals("+")){
+                result += Integer.parseInt(num[2]);
+            }else {
+                result -= Integer.parseInt(num[2]);
             }
             answer[i] = result == Integer.parseInt(num[4]) ? "O" : "X";
         }

b138feb48b76f66f768b564f0acf05bfe21e607c
milliee <112637496+millieeway@users.noreply.github.com>
2023-01-17 23:15:31 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,15 +1,14 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        String answer = "fail";
         for(int i = 0; i < db.length; i++){
             if(id_pw[0].equals(db[i][0])){
                 if(id_pw[1].equals(db[i][1])){
-                    answer = "login";
+                    return "login";
                 }else{
-                    answer = "wrong pw";
+                    return "wrong pw";
                 }
             }
         }
-        return answer;
+        return "fail";
     }
 }
